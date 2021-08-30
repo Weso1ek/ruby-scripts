0222663764e0cf8692432cfa01c656dadbc73f7b
Sebastian Wesołowski <Sebastian.Wesolowski@grupawp.pl>
2021-08-30 23:05:22 +0200

    Lesson 3 - output input

---
+++ lesson-3-output.rb
@@ -0,0 +1,27 @@
+irb(main):001:0> print "Learning to code is FUN!"
+Learning to code is FUN!=> nil
+
+irb(main):002:0> print "1234"
+1234=> nil
+
+irb(main):001:0> print "Hello World"; print "I love drinking coffee"
+Hello WorldI love drinking coffee=> nil
+
+irb(main):002:0> puts "Hello World"; puts "I love drinking coffee"
+Hello World
+I love drinking coffee
+=> nil
+
+irb(main):001:0> gets
+The Odin Project
+=> "The Odin Project\n"
+
+
+
+irb(main):001:0> new_string = gets.chomp
+This is a sentence.
+          => "This is a sentence."
+
+irb(main):002:0> puts new_string
+This is a sentence.
+          => nil
